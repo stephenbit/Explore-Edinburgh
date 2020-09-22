1acf98eadb6b1f235243d7ca85550c2e7c77e6d7
Stephen Ramsay <rabrandom@gmail.com>
2020-09-22 16:34:25 +0100

    mongodb running seeds

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "../App.css";
 
 function Posts({ posts }) {
   const postLis = posts.map((post) => {
     return (
-      <li key={post.id}>
+      <li key={post._id}>
         <h1>{post.title}</h1>
+        
         <p>{post.body}</p>
       </li>
     );
--- server/index.js
+++ server/index.js
@@ -1,41 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
 const port = 8080;
 
 app.use(cors());
 
 app.get("/posts", (req, res) => {
-  res.json({
-    posts: [
-      {
-        id: "00001",
-        title: "Welcome to Explore Edinburgh(express test 1)",
-        featuredPhoto: "43242552",
-        date: "14th September 2020, 14:26",
-        tags: ["edinburgh", "explore edinburgh"],
-        body:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, quam vel porta porta, elit mauris viverra diam, et pharetra nisl lorem eget neque. Integer porttitor turpis cursus tellus vulputate cursus. Etiam ullamcorper vehicula urna, ac commodo purus dapibus eu. Duis fermentum turpis at hendrerit dictum. Donec in sapien id urna maximus accumsan. Quisque vel augue vel quam molestie sollicitudin. Quisque a purus et odio tempus viverra. Sed sodales nisi vel diam auctor blandit. Aenean ac lectus non enim facilisis sollicitudin vitae non nisl. Nullam vitae mollis dolor. Nulla sit amet mauris vitae mauris convallis scelerisque. Integer consectetur feugiat lobortis. Ut sed nunc quis sapien dictum condimentum nec nec arcu. Donec dictum a magna eget tincidunt. Pellentesque ac leo lobortis, lobortis purus in, molestie erat. Nullam sagittis, erat sed rutrum accumsan, purus sem efficitur enim, ut tempus massa magna a tortor. <br/><br/>Praesent sit amet maximus quam. Pellentesque augue nisi, convallis non ipsum sit amet, semper ornare mi. Nunc mattis tellus luctus augue cursus, non porta lectus porta. Vestibulum lacinia urna metus, in volutpat est blandit vitae. Nam non mollis sapien, aliquam gravida mi. Sed interdum in nisl vel egestas. Donec fermentum, enim sed sagittis dapibus, lectus sem mollis mi, nec placerat lectus orci nec lectus. Sed gravida varius nulla id posuere. Fusce cursus tortor in massa tristique blandit. Aenean posuere velit vitae tortor dictum aliquam. Vestibulum commodo, nunc at pretium porta, mauris erat mattis purus, nec dapibus est orci non arcu. Praesent in ligula in metus vulputate rhoncus eu eu turpis. Maecenas dapibus leo leo. Etiam suscipit finibus turpis, iaculis varius risus egestas sed. Curabitur nec sapien id arcu faucibus luctus.",
-      },
-      {
-        id: "00002",
-        title: "Welcome to Explore Edinburgh(express test 1)",
-        featuredPhoto: "43242552",
-        date: "14th September 2020, 14:26",
-        tags: ["edinburgh", "explore edinburgh"],
-        body:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, quam vel porta porta, elit mauris viverra diam, et pharetra nisl lorem eget neque. Integer porttitor turpis cursus tellus vulputate cursus. Etiam ullamcorper vehicula urna, ac commodo purus dapibus eu. Duis fermentum turpis at hendrerit dictum. Donec in sapien id urna maximus accumsan. Quisque vel augue vel quam molestie sollicitudin. Quisque a purus et odio tempus viverra. Sed sodales nisi vel diam auctor blandit. Aenean ac lectus non enim facilisis sollicitudin vitae non nisl. Nullam vitae mollis dolor. Nulla sit amet mauris vitae mauris convallis scelerisque. Integer consectetur feugiat lobortis. Ut sed nunc quis sapien dictum condimentum nec nec arcu. Donec dictum a magna eget tincidunt. Pellentesque ac leo lobortis, lobortis purus in, molestie erat. Nullam sagittis, erat sed rutrum accumsan, purus sem efficitur enim, ut tempus massa magna a tortor. <br/><br/>Praesent sit amet maximus quam. Pellentesque augue nisi, convallis non ipsum sit amet, semper ornare mi. Nunc mattis tellus luctus augue cursus, non porta lectus porta. Vestibulum lacinia urna metus, in volutpat est blandit vitae. Nam non mollis sapien, aliquam gravida mi. Sed interdum in nisl vel egestas. Donec fermentum, enim sed sagittis dapibus, lectus sem mollis mi, nec placerat lectus orci nec lectus. Sed gravida varius nulla id posuere. Fusce cursus tortor in massa tristique blandit. Aenean posuere velit vitae tortor dictum aliquam. Vestibulum commodo, nunc at pretium porta, mauris erat mattis purus, nec dapibus est orci non arcu. Praesent in ligula in metus vulputate rhoncus eu eu turpis. Maecenas dapibus leo leo. Etiam suscipit finibus turpis, iaculis varius risus egestas sed. Curabitur nec sapien id arcu faucibus luctus.",
-      },
-      {
-        id: "00003",
-        title: "Welcome to Explore Edinburgh(express test 1)",
-        featuredPhoto: "43242552",
-        date: "14th September 2020, 14:26",
-        tags: ["edinburgh", "explore edinburgh"],
-        body:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum, quam vel porta porta, elit mauris viverra diam, et pharetra nisl lorem eget neque. Integer porttitor turpis cursus tellus vulputate cursus. Etiam ullamcorper vehicula urna, ac commodo purus dapibus eu. Duis fermentum turpis at hendrerit dictum. Donec in sapien id urna maximus accumsan. Quisque vel augue vel quam molestie sollicitudin. Quisque a purus et odio tempus viverra. Sed sodales nisi vel diam auctor blandit. Aenean ac lectus non enim facilisis sollicitudin vitae non nisl. Nullam vitae mollis dolor. Nulla sit amet mauris vitae mauris convallis scelerisque. Integer consectetur feugiat lobortis. Ut sed nunc quis sapien dictum condimentum nec nec arcu. Donec dictum a magna eget tincidunt. Pellentesque ac leo lobortis, lobortis purus in, molestie erat. Nullam sagittis, erat sed rutrum accumsan, purus sem efficitur enim, ut tempus massa magna a tortor. <br/><br/>Praesent sit amet maximus quam. Pellentesque augue nisi, convallis non ipsum sit amet, semper ornare mi. Nunc mattis tellus luctus augue cursus, non porta lectus porta. Vestibulum lacinia urna metus, in volutpat est blandit vitae. Nam non mollis sapien, aliquam gravida mi. Sed interdum in nisl vel egestas. Donec fermentum, enim sed sagittis dapibus, lectus sem mollis mi, nec placerat lectus orci nec lectus. Sed gravida varius nulla id posuere. Fusce cursus tortor in massa tristique blandit. Aenean posuere velit vitae tortor dictum aliquam. Vestibulum commodo, nunc at pretium porta, mauris erat mattis purus, nec dapibus est orci non arcu. Praesent in ligula in metus vulputate rhoncus eu eu turpis. Maecenas dapibus leo leo. Etiam suscipit finibus turpis, iaculis varius risus egestas sed. Curabitur nec sapien id arcu faucibus luctus.",
-      },
-    ],
+  MongoClient.connect("mongodb://localhost:27017").then((client) => {
+    const db = client.db("explore_edinburgh");
+    const postsCollection = db.collection("posts");
+    postsCollection
+      .find({})
+      .toArray()
+      .then((posts) => res.json({posts: posts}));
   });
 });
 
