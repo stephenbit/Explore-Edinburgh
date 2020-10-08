387c0060b798fb24b5a2afe4a1fea7ee6224e4bc
Stephen Ramsay <rabrandom@gmail.com>
2020-10-08 11:22:52 +0100

    pushed to dockerhub

--- server/server.js
+++ server/server.js
@@ -1,38 +1,28 @@
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-// // const MongoClient = require("mongodb").MongoClient;
-// const port = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// const MongoClient = require("mongodb").MongoClient;
+const port = 8080;
 
-// app.use(cors());
+app.use(cors());
 
-// app.get("/posts", (req, res) => {
-//   MongoClient.connect("mongodb://root:example@mongo:27017").then((client) => {
-//     const db = client.db("explore_edinburgh");
-//     const postsCollection = db.collection("posts");
-//     postsCollection
-//       .find({})
-//       .toArray()
-//       .then((posts) => res.json({ posts: posts }));
-//   });
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-
-const express = require('express')
-const app = express()
-const port = 8080
-
-app.get('/api/test', (req, res) => {
-  res.send({"testworks":true})
-})
+app.get("/posts", (req, res) => {
+  MongoClient.connect("mongodb://root:example@mongo:27017").then((client) => {
+    const db = client.db("explore_edinburgh");
+    const postsCollection = db.collection("posts");
+    postsCollection
+      .find({})
+      .toArray()
+      .then((posts) => res.json({ posts: posts }));
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
 
-app.get('/', (req, res) => {
-  res.send({"testworks":true})
-})
+//tests
+
+app.get("/api/test", (req, res) => {
+  res.send({ testworks: true });
+});
