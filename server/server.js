96833f4fda9425b2b75b273ea39ce04e195f3da3
Stephen Ramsay <rabrandom@gmail.com>
2020-10-13 23:08:10 +0100

    repaired server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const port = 8080;
 app.use(cors());
 
 app.get("/posts", (req, res) => {
-  MongoClient.connect(`mongodb://${process.env.MONGODB_USERRNAME}:${process.env.MONGO_PW}example}@mongo:27017`).then((client) => {
+  MongoClient.connect(`mongodb://root:example@mongo:27017`).then((client) => {
     const db = client.db("explore_edinburgh");
     const postsCollection = db.collection("posts");
     postsCollection
