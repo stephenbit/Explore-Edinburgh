5506f6909a46e040ff552cdf9a4a0e949f2955c6
Stephen Ramsay <rabrandom@gmail.com>
2020-10-14 21:45:43 +0100

    add a new route and changed component names to articles

--- server/server.js
+++ server/server.js
@@ -1,28 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
+
 const port = 8080;
 
 app.use(cors());
+app.use(bodyParser.json());
 
-app.get("/posts", (req, res) => {
-  MongoClient.connect("mongodb://root:example@mongo:27017").then((client) => {
-    const db = client.db("explore_edinburgh");
-    const postsCollection = db.collection("posts");
-    postsCollection
+MongoClient.connect("mongodb://root:example@mongo:27017", {
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db("explore_edinburgh");
+  const articlesCollection = db.collection("articles");
+
+  app.get("/articles", (req, res) => {
+    articlesCollection
       .find({})
       .toArray()
-      .then((posts) => res.json({ posts: posts }));
+      .then((articles) => res.json({ articles: articles }));
+  });
+
+  app.get("/articles/:id", (req, res) => {
+    const id = req.params.id;
+    articlesCollection
+      .findOne({ _id: ObjectID(id) })
+      .then((article) => res.json(article));
   });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
-//tests
-
+// test
 app.get("/api/test", (req, res) => {
   res.send({ testworks: true });
+});
+// test
+
+app.listen(port, () => {
+  console.log(`Explore Edinburgh listening at http://52.56.174.117:${port}`);
 });
