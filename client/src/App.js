0d33018117d54b03fa51d256c210515a753ee270
Stephen Ramsay <rabrandom@gmail.com>
2020-10-15 11:13:42 +0100

    add new fetch to test

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,30 @@ import Navbar from "./components/Navbar.js";
 import Articles from "./components/Articles.js";
 import SingleArticle from "./components/SingleArticle.js";
 
+const ipAddress = "localhost";
+// const ipAddress = "52.56.174.117"
+
 function App() {
   const [articles, setArticles] = useState([]);
+  const [singleArticle, setSingleArticle] = useState([]);
 
   useEffect(() => {
-    fetch("http://52.56.174.117:8080/articles")
+    fetch(`http://${ipAddress}:8080/articles`)
       .then((res) => res.json())
       .then((data) => setArticles(data.articles));
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://${ipAddress}:8080/articles/5f86efd2f876393a60cf362d`)
+      .then((res) => res.json())
+      .then((data) => setSingleArticle(data.singleArticle));
   }, []);
 
   return (
     <>
       <Navbar />
-      <Articles articles={articles} />
-      <SingleArticle/>
+      <Articles articles={articles} singleArticle={singleArticle} />
+      <SingleArticle />
     </>
   );
 }
