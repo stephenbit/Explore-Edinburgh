3d7269f3b3fd1276a959daf49966fc93c261b1b3
Stephen Ramsay <rabrandom@gmail.com>
2020-10-15 11:12:25 +0100

    add variables to control mongo connect

--- server/server.js
+++ server/server.js
@@ -4,32 +4,34 @@ const app = express();
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
+const mongoClientDetails = "mongodb://localhost:27017";
+// const mongoClientDetails = "mongodb://root:example@mongo:27017"
 
 const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-MongoClient.connect("mongodb://root:example@mongo:27017", {
-  useUnifiedTopology: true,
-}).then((client) => {
-  const db = client.db("explore_edinburgh");
-  const articlesCollection = db.collection("articles");
+MongoClient.connect(`${mongoClientDetails}`, { useUnifiedTopology: true }).then(
+  (client) => {
+    const db = client.db("explore_edinburgh");
+    const articlesCollection = db.collection("articles");
 
-  app.get("/articles", (req, res) => {
-    articlesCollection
-      .find({})
-      .toArray()
-      .then((articles) => res.json({ articles: articles }));
-  });
+    app.get("/articles", (req, res) => {
+      articlesCollection
+        .find({})
+        .toArray()
+        .then((articles) => res.json({ articles: articles }));
+    });
 
-  app.get("/articles/:id", (req, res) => {
-    const id = req.params.id;
-    articlesCollection
-      .findOne({ _id: ObjectID(id) })
-      .then((article) => res.json(article));
-  });
-});
+    app.get("/articles/:id", (req, res) => {
+      const id = req.params.id;
+      articlesCollection
+        .findOne({ _id: ObjectID(id) })
+        .then((article) => res.json(article));
+    });
+  }
+);
 
 // test
 app.get("/api/test", (req, res) => {
