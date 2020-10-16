0ef104bb4aaafebe9a1cd624e04bc7e793fb2bc2
Stephen Ramsay <rabrandom@gmail.com>
2020-10-16 23:40:10 +0100

    Article render

--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -2,7 +2,11 @@ import React from "react";
 import "../App.css";
 
 function Article({ article }) {
-
-  return <h1>the {article.body}</h1>
+  return (
+    <>
+    <h1>{article.title}</h1>
+      <p>{article.body}</p>
+    </>
+  );
 }
 export default Article;
