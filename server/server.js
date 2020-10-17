71b6a3578c21326f378bb40f99ca7a5a3fc65cc1
Stephen Ramsay <rabrandom@gmail.com>
2020-10-17 23:38:03 +0100

    removed helpers again

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 // const mongoClientDetails = "mongodb://localhost:27017";
-const mongoClientDetails = "mongodb://root:example@mongo:27017"
+const mongoClientDetails = "mongodb://root:example@mongo:27017";
 
 const port = 8080;
 
